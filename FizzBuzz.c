1b6a0e038d983652e627b031e02d1ff5efa217fa
d-tomic23 <35478402+d-tomic23@users.noreply.github.com>
2018-01-15 23:35:47 -0500

    Create FizzBuzz.c

---
+++ FizzBuzz.c
@@ -0,0 +1,29 @@
+/**
+ File: fizzbuzz.c
+ Fizz Buzz!
+*/
+
+#include <stdio.h>
+
+void printFizzBuzz() {
+  for (int i=1; i<=20; i++) {
+    if ((i%3==0) && (i%5==0)) {
+      printf("FizzBuzz\n");
+    }
+    else if (i%3==0) {
+      printf("Fizz\n");
+    }
+    else if (i%5==0) {
+      printf("Buzz\n");
+    }
+    else {
+      printf("%d\n", i);
+    }
+  }
+}
+
+int main(void) {
+  printFizzBuzz();
+  
+  return 0;
+}
